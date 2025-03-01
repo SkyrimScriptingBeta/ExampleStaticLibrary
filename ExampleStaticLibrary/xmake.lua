7bb63bcb3e5c15059d76bd1b8f5f1f31012ca91c
Mrowr Purr <mrowr.purr@gmail.com>
2025-03-01 15:10:30 -0800

    Hmm!

--- ExampleStaticLibrary/xmake.lua
+++ ExampleStaticLibrary/xmake.lua
@@ -1,17 +1,20 @@
+add_rules("mode.debug", "mode.release")
+
+set_languages("c++23")
+
 if has_config("build_example") then
-    add_rules("mode.debug", "mode.release")
-    set_languages("c++23")
     add_repositories("SkyrimScripting https://github.com/SkyrimScripting/Packages.git")
     add_requires("skyrim-commonlib-ae")
 end
 
 if has_config("commonlib") then
-    commonlib_package = get_config("commonlib")
-    print("[Building] Using CommonLib package: " .. commonlib_package)
+    print("[Building] Using CommonLib package: " .. get_config("commonlib"))
 end
 
 target("ExampleStaticLibrary")
     set_kind("static")
     add_files("src/*.cpp")
     add_includedirs("include", { public = true }) -- Your library's own include path
-    add_packages(commonlib_package, { public = true }) -- Ensure it propagates
+    if has_config("build_example") then
+        add_packages(get_config("commonlib"), { public = true })
+    end
