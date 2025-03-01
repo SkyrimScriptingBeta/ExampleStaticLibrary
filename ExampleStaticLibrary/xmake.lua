7687cbc8985eab4e86695d28f6e03e515cb27003
Mrowr Purr <mrowr.purr@gmail.com>
2025-03-01 15:15:54 -0800

    move get_config out of package()

--- ExampleStaticLibrary/xmake.lua
+++ ExampleStaticLibrary/xmake.lua
@@ -7,8 +7,9 @@ if has_config("build_example") then
     add_requires("skyrim-commonlib-ae")
 end
 
-if has_config("commonlib") then
-    print("[Building] Using CommonLib package: " .. get_config("commonlib"))
+commonlib_package = get_config("commonlib")
+if commonlib_package then
+    print("[Building] Using CommonLib package: " .. commonlib_package)
 end
 
 target("ExampleStaticLibrary")
@@ -16,5 +17,5 @@ target("ExampleStaticLibrary")
     add_files("src/*.cpp")
     add_includedirs("include", { public = true }) -- Your library's own include path
     if has_config("build_example") then
-        add_packages(get_config("commonlib"), { public = true })
+        add_packages(commonlib_package, { public = true })
     end
